72e59babf57c1e0b335084a8cc1826f3a27831b1
Gavin Beatty <public@gavinbeatty.com>
2021-11-25 19:27:46 +0000

    use mmctl to detect running version, since mattermost version has been replaced with mmctl system version in v6.0

--- upgrade/00-latest.sh
+++ upgrade/00-latest.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 cd /
-echo Installed:
-installed="$(sudo -u mattermost /opt/mattermost/bin/mattermost version 2>/dev/null | sed -n 's/^[A-Z]/  &/p')"
-printf %s\\n "$installed"
+echo Running:
+# fmt.Sprintf("  Server version %v.%v.%v.%v", model.CurrentVersion, model.BuildNumber, c.App.ClientConfigHash(), c.App.Srv().License() != nil)
+running="$(sudo -u mattermost /opt/mattermost/bin/mmctl --local --strict system version 2>/dev/null | sed -n 's/^[A-Z]/  &/p')"
+runningvers="$(printf %s\\n "$running" | sed -e 's/^\s*Server version \(.*\)\.[a-f0-9]\{32\}\..*/\1/')"
+runningdots="$(printf %s\\n "$runningvers" | tr -d 'a-zA-Z0-9\n' | wc -c)"
+runningver="$(printf %s\\n "$runningvers" | awk -F. "{print $(seq 1 $((runningdots/2)) | xargs -rd\\n printf '$%d "." ') \$$((runningdots/2 + 1))}")"
+if test -z "$runningver" ; then
+    echo "Could not extract running version info from '$running'." >&2 ; exit 1
+fi
+printf "  Running version: %s\\n" "$runningver"
+runningent="$(printf %s\\n "$running" | sed -e 's/^.*\.[a-f0-9]\{32\}\.\(true\|false\)$/\1/')"
+case "$runningent" in
+    true|false) true ;;
+    *) echo "Could not extract running enterprise info from '$running'." >&2 ; exit 1 ;;
+esac
+printf "  Running enterprise: %s\\n" "$runningent"
 echo Latest:
 deployhtml="$(curl -fsSL https://mattermost.com/deploy/)"
 deployver="$(printf %s\\n "$deployhtml" | sed -n 's/^\s*\(<[^<]*class="release-text-left-latest"\)/  \1/p' | sed -e 's/.*>\s*\(.*\)\s*<.*/\1/')"
 printf "  Deploy version: %s\\n" "$deployver"
-installedvermajor="${installedver%%.*}"
+runningvermajor="${runningver%%.*}"
 deploydate="$(printf %s\\n "$deployhtml" | sed -n 's/^\s*\(<[^<]*class="release-text-left-date"\)/  \1/p' | sed -e 's/.*>\s*\(.*\)\s*<.*/\1/')"
 printf "  Deploy date: %s\\n" "$deploydate"
 vahtml="$(curl -fsSL https://docs.mattermost.com/upgrade/version-archive.html)"
-vahtmlsubset="$(printf %s\\n "$vahtml" | grep '<code\b.*\bhttps://releases\.mattermost\.com/[^d/][^/]*/mattermost-team-'"$installedvermajor" -A1)"
+vahtmlsubset="$(printf %s\\n "$vahtml" | grep '<code\b.*\bhttps://releases\.mattermost\.com/[^d/][^/]*/mattermost-team-'"$runningvermajor" -A1)"
 archiveurl="$(printf %s\\n "$vahtmlsubset" | sed -n '1s#.*\b\(https://releases\.mattermost\.com/[^d/][^/]*/mattermost-team-[^"<>]*linux-amd64\.\(tar\.\|t\)\(gz\|bz2\|xz\|zst\)\).*#\1#p')"
 archiveurlver="$(printf %s\\n "$archiveurl" | sed -e 's#.*\bhttps://releases\.mattermost\.com/\([^d/][^/]*\).*#\1#')"
 archivesum="$(printf %s\\n "$vahtmlsubset" | sed -En '2s#.*[^0-9a-fA-F]([0-9a-fA-F]{64})[^0-9a-fA-F].*#\1#p')"
@@ -51,15 +64,10 @@ if test -z "$archivesum" ; then
 fi
 
 
-installedent="$(printf %s\\n "$installed" | sed -n 's/^\s*Build Enterprise Ready:\s*//p')"
-installedver="$(printf %s\\n "$installed" | sed -n '1s/^.*:\s*//p')"
-if test -z "$installedent" || test -z "$installedver" ; then
-    echo Could not extract installed version info. >&2 ; exit 1
-fi
 if test "$deployver" != "$archiveurlver" && test "$deployver.0" != "$archiveurlver" ; then
     echo "Warning: Mismatch between latest deploy version '$deployver' and latest archive URL version '$archiveurlver'." >&2
 fi
-if test "$installedent" = false && test "$installedver" = "$archiveurlver" ; then
+if test "$runningent" = false && test "$runningver" = "$archiveurlver" ; then
     echo Up to date. ; exit 0
 fi
 archive="$(basename "$archiveurl")"

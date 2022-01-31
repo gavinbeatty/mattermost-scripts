8b6453cdae7202fd1d9abe07e6850e2f69228e4f
Gavin Beatty <public@gavinbeatty.com>
2022-01-31 22:41:57 +0000

    backup all of /etc/nginx as well

--- upgrade/01-backup.sh
+++ upgrade/01-backup.sh
@@ -1,6 +1,10 @@
 #!/bin/dash
 set -eu
 set -x
+dt="$(date -Iseconds)"
+own="$(id -u):$(id -g)"
+nginxtar="$HOME/backup/mattermost/$dt-nginx-etc.tar.zst"
+(cd / && sudo bsdtar -LH --zstd -cf "$nginxtar" ./etc/nginx && sudo chown "$own" "$nginxtar")
 sudo systemctl start mattermost-backup-pg.service
 sudo systemctl start mattermost-backup-config.service
 sudo systemctl start mattermost-backup-data.service

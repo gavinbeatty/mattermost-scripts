35241f8138c9af879d072d4686cc25f8b0a78062
Gavin Beatty <public@gavinbeatty.com>
2022-02-05 17:20:16 +0000

    use no-deref in tar chown

--- 01-backup.sh
+++ 01-backup.sh
@@ -4,7 +4,7 @@ set -x
 dt="$(date -Iseconds)"
 own="$(id -u):$(id -g)"
 nginxtar="$HOME/backup/mattermost/$dt-nginx-etc.tar.zst"
-(cd / && sudo bsdtar -LH --zstd -cf "$nginxtar" ./etc/nginx && sudo chown "$own" "$nginxtar")
+(cd / && sudo bsdtar -LH --zstd -cf "$nginxtar" ./etc/nginx && sudo chown -h "$own" "$nginxtar")
 sudo systemctl start mattermost-backup-pg.service
 sudo systemctl start mattermost-backup-config.service
 sudo systemctl start mattermost-backup-data.service
